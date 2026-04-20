0e4d416db100421037b75e3dd30086a4248edac9
James.I <jimmyjitz@gmail.com>
2026-04-20 21:15:40 +0100

    Adjust timeout for envelope click event
    Reduced timeout duration for envelope click action.

--- script.js
+++ script.js
@@ -4,23 +4,14 @@ document.addEventListener("DOMContentLoaded", function () {
     const envelopeScreen = document.getElementById("envelopeScreen");
     const inviteScreen = document.getElementById("inviteScreen");
 
-    if (!envelope) {
-        console.log("Envelope NOT found ❌");
-        return;
-    }
-
-    console.log("Envelope found ✅");
-
     envelope.addEventListener("click", function () {
-
-        console.log("Envelope clicked ✅");
 
         envelope.classList.add("open");
 
         setTimeout(() => {
             envelopeScreen.classList.add("hide");
             inviteScreen.classList.add("show");
-        }, 2000);
+        }, 1500);
 
     });
 
